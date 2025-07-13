7a2e9de0b2af3093466552930fbbe29ba8fd3b51
Amolcode <amollokhande2303@gmail.com>
2025-07-13 10:31:45 +0530

    amol is adding changes

---
+++ gsap.js
@@ -0,0 +1,54 @@
+// gsap.to("#box1",{
+// x:500,
+// duration:2,
+// delay:1,
+// scale:-1.2,
+// borderRadius:"50%",
+// rotation:360,
+// repeat:1,
+// yoyo:
+
+// })
+
+// gsap.to("#box2",{
+// x:500,
+// duration:2,
+// delay:3,
+// scale:-1.2,
+// borderRadius:"50%",
+// rotation:360,
+
+// })
+
+// gsap.to("#box3",{
+// x:500,
+// duration:2,
+// delay:5,
+// scale:-1.2,
+// borderRadius:"50%",
+// rotation:360
+// })
+
+
+// TIMELINE IN GSAP 
+
+// var tl = gsap.timeline();
+
+// tl.to("#box1",{
+//     x:1000,
+//     rotation:360,
+//     duration:1.5,
+    
+// })
+
+// tl.to("#box2",{
+//     x:1000,
+//     scale:1.25,
+//     duration:1.5,
+   
+// })
+
+
+// NAVBAR GSAP code START FROM HERE
+
+var tl = gsap.timeline();
